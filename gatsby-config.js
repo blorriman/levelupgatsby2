c50a7876590a00afe8b329608b705e39a1e6354e
Bob Lorriman <blorriman@gmail.com>
2018-08-27 14:13:02 -0700

    config for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'img',
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/static/assets`
       }
     },
     // {
